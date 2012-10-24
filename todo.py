5d3b972bf9ba1900f62a51e8ffd9a9e188cb3f5d
Yuta Kawabe <yuyu3165@gmail>
2012-10-24 23:49:34 +0900

    add QTreeView

--- todo.py
+++ todo.py
@@ -3,11 +3,24 @@ from PyQt4 import QtCore, QtGui
 
 class Window(QtGui.QMainWindow):
     def __init__(self):
-        QtGui.QMainWindow.__inin__(self)
+        super(Window, self).__init__()
         # x, y, width, height
         self.setGeometry(0, 0, 340, 460)
         self.setWindowTitle('TODO')
-        
+        # tree view
+        self.treeView = QtGui.QTreeView()
+        self.stdItemModel = QtGui.QStandardItemModel(0, 3)
+        self.stdItemModel.setHeaderData(0, QtCore.Qt.Horizontal, 'Task')
+        self.stdItemModel.setHeaderData(1, QtCore.Qt.Horizontal, 'Data')
+        self.stdItemModel.setHeaderData(2, QtCore.Qt.Horizontal, 'Tags')
+        self.treeView.setModel(self.stdItemModel)
+        # layout and central widget
+        hbox = QtGui.QHBoxLayout()
+        hbox.addWidget(self.treeView)
+        self.widget = QtGui.QWidget()
+        self.widget.setLayout(hbox)
+        self.setCentralWidget(self.widget)
+
     def closeEvent(self, event):
         # widget, title, message, button1 | button2, default focus
         reply = QtGui.QMessageBox.question(self,
