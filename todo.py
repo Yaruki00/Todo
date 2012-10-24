b5c9a93ae6021ea4c5c4628c93001909e60d3422
Yuta Kawabe <yuyu3165@gmail>
2012-10-24 15:03:22 +0900

    show message box when closing

--- todo.py
+++ todo.py
@@ -4,9 +4,22 @@ from PyQt4 import QtCore, QtGui
 class Window(QtGui.QMainWindow):
     def __init__(self):
         QtGui.QMainWindow.__inin__(self)
+        # x, y, width, height
         self.setGeometry(0, 0, 340, 460)
         self.setWindowTitle('TODO')
-
+        
+    def closeEvent(self, event):
+        # widget, title, message, button1 | button2, default focus
+        reply = QtGui.QMessageBox.question(self,
+                                           'Confirmation',
+                                           "Are you sure to quit?",
+                                           QtGui.QMessageBox.Yes |
+                                           QtGui.QMessageBox.No,
+                                           QtGui.QMessageBox.No)
+        if reply == QtGui.QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
 
 def main():
     app = QtGui.QApplication(sys.argv)
