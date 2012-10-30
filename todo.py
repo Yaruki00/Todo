3a6d4516e1d1b1eb1a2ba02d26c892bf3f1d0cee
Yuta Kawabe <yuyu3165@gmail>
2012-10-30 16:20:38 +0900

    add deletetask action

--- todo.py
+++ todo.py
@@ -26,18 +26,25 @@ class Window(QtGui.QMainWindow):
         self.setCentralWidget(self.widget)
         # status bar
         self.statusBar().showMessage('ready')
-        # action
+        # exit action
         exitAction = QtGui.QAction(QtGui.QIcon('exit.png'), 'Exit', self)
         exitAction.setShortcut('Ctrl+Q')
         exitAction.setStatusTip('Exit application')
         exitAction.triggered.connect(self.close)
+        # delete action
+        self.deleteAction = QtGui.QAction(QtGui.QIcon('delete.png'), 'Delete', self)
+        self.deleteAction.setShortcut('Ctrl+R')
+        self.deleteAction.setStatusTip('Delete selected task')
+        self.deleteAction.triggered.connect(self.delete)
         # menu bar
         menubar = self.menuBar()
-        menubar.addMenu('&File')
-        menubar.addMenu('&Edit')
+        fileMenu = menubar.addMenu('&File')
+        editMenu = menubar.addMenu('&Edit')
+        editMenu.addAction(self.deleteAction)
         # tool bar
         toolbar = self.addToolBar('Exit')
         toolbar.addAction(exitAction)
+        toolbar.addAction(self.deleteAction)
         # load data from DB and add in tree widget
         for task in todoDB.Task.query.all():
             tags = ','.join([t.name for t in task.tags])
@@ -48,9 +55,14 @@ class Window(QtGui.QMainWindow):
             else:
                 item.setCheckState(0, QtCore.Qt.Unchecked)
             self.treeWidget.addTopLevelItem(item)
+        # connect
         self.treeWidget.itemChanged.connect(self.on_treeWidget_itemChanged)
+        self.treeWidget.currentItemChanged.connect(self.on_treeWidget_currentItemChanged)
+        # fix width
         for column in range(0, self.treeWidget.columnCount()):
             self.treeWidget.resizeColumnToContents(column)
+        # set current item
+        self.treeWidget.setCurrentItem(None)
 
     def on_treeWidget_itemChanged(self, item, column):
         if item.checkState(0):
@@ -58,6 +70,28 @@ class Window(QtGui.QMainWindow):
         else:
             item.task.done = False
         todoDB.saveData()
+
+    def on_treeWidget_currentItemChanged(self, current, previous):
+        if current:
+            self.deleteAction.setEnabled(True)
+        else:
+            self.deleteAction.setEnabled(False)
+
+    def delete(self):
+        selectedItem = self.treeWidget.currentItem()
+        if not selectedItem:
+            return
+        reply = QtGui.QMessageBox.question(self,
+                                   'Confirmation',
+                                   "Are you sure to delete?",
+                                   QtGui.QMessageBox.Yes |
+                                   QtGui.QMessageBox.No,
+                                   QtGui.QMessageBox.No)
+        if reply == QtGui.QMessageBox.Yes:
+            selectedItem.task.delete()
+            todoDB.saveData()
+            self.treeWidget.takeTopLevelItem(
+                self.treeWidget.indexOfTopLevelItem(selectedItem))
 
     def closeEvent(self, event):
         # widget, title, message, button1 | button2, default focus
