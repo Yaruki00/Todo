6b6eb11425c1ac244ef29ed89e3423e641fe53ba
Yuta Kawabe <yuyu3165@gmail>
2012-10-31 19:03:34 +0900

    create edit method

--- editor.py
+++ editor.py
@@ -9,29 +9,44 @@ class Editor(QtGui.QWidget):
         vbox = QtGui.QVBoxLayout()
         form = QtGui.QFormLayout()
         # form components
-        taskLabel = QtGui.QLabel('&Task:')
-        taskLineEdit = QtGui.QLineEdit()
-        taskLabel.setBuddy(taskLineEdit)
-        doneCheck = QtGui.QCheckBox('&Finished')
-        dateLabel = QtGui.QLabel('&Due Date:')
-        dateTimeEdit = QtGui.QDateTimeEdit()
-        dateTimeEdit.setCalendarPopup(True)
-        dateLabel.setBuddy(dateTimeEdit)
-        tagLabel = QtGui.QLabel('&Task:')
-        tagLineEdit = QtGui.QLineEdit()
-        tagLabel.setBuddy(tagLineEdit)
+        self.taskLabel = QtGui.QLabel('&Task:')
+        self.taskLineEdit = QtGui.QLineEdit()
+        self.taskLabel.setBuddy(self.taskLineEdit)
+        self.doneCheck = QtGui.QCheckBox('&Finished')
+        self.dateLabel = QtGui.QLabel('&Due Date:')
+        self.dateTimeEdit = QtGui.QDateTimeEdit()
+        self.dateTimeEdit.setCalendarPopup(True)
+        self.dateLabel.setBuddy(self.dateTimeEdit)
+        self.tagLabel = QtGui.QLabel('&Task:')
+        self.tagLineEdit = QtGui.QLineEdit()
+        self.tagLabel.setBuddy(self.tagLineEdit)
         # add components to form
-        form.addRow(taskLabel, taskLineEdit)
-        form.addRow(None, doneCheck)
-        form.addRow(dateLabel, dateTimeEdit)
-        form.addRow(tagLabel, tagLineEdit)
+        form.addRow(self.taskLabel, self.taskLineEdit)
+        form.addRow(None, self.doneCheck)
+        form.addRow(self.dateLabel, self.dateTimeEdit)
+        form.addRow(self.tagLabel, self.tagLineEdit)
         vbox.addLayout(form)
         self.setLayout(vbox)
         # define tab order
-        self.setTabOrder(taskLabel, doneCheck)
-        self.setTabOrder(doneCheck, dateLabel)
-        self.setTabOrder(dateLabel, tagLabel)
-        self.setTabOrder(tagLabel, taskLabel)
+        self.setTabOrder(self.taskLabel, self.doneCheck)
+        self.setTabOrder(self.doneCheck, self.dateLabel)
+        self.setTabOrder(self.dateLabel, self.tagLabel)
+        self.setTabOrder(self.tagLabel, self.taskLabel)
+        # init item
+        self.item = None
+
+    def edit(self, item):
+        self.item = item
+        self.taskLineEdit.setText(self.item.task.text)
+        self.doneCheck.setChecked(self.item.task.done)
+        dt = self.item.task.date
+        if dt:
+            self.dateTimeEdit.setDate(QtCore.QDate(dt.year, dt.month, dt.day))
+            self.dateTimeEdit.setTime(QtCore.QTime(dt.hour, dt.minute))
+        else:
+            self.dateTimeEdit.setDateTime(QtCore.QTime())
+        self.tagLineEdit.setText(','.join(t.name for t in self.item.task.tags))
+        self.show()
 
 def main():
     app = QtGui.QApplication(sys.argv)
